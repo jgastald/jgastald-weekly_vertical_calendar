c073679c7f2170de5ef0efb65b7492a231d21ba1
Jorge Gastaldi <jgastald@gmail.com>
2011-11-23 02:23:48 -0200

    Getting things in shape

--- lib/generators/weekly_vertical_calendar/weekly_vertical_calendar_generator.rb
+++ lib/generators/weekly_vertical_calendar/weekly_vertical_calendar_generator.rb
@@ -2,19 +2,21 @@ class WeeklyVerticalCalendarGenerator < Rails::Generators::NamedBase
   source_root File.expand_path('../templates', __FILE__)
   
   
-    def initialize(args, runtime_options = {})
-      super
-      usage if args.length > 0 and args.length < 2
-      @class_name = (args.shift || "event").underscore
-      @view_name = (args.shift || "calendar").underscore
-    end
+  #    def initialize(args, runtime_options = {})
+#      super
+#      usage if args.length > 0 and args.length < 2
+#      @class_name = (args.shift || "event").underscore
+#      @view_name = (args.shift || "calendar").underscore
+      
+      #    end
   
   
   def manifest
-    record do |m|
-      m.template "index.html.erb", File.join("app/views", @view_name, "index.html.erb")
-      m.template "_wcal.erb", File.join("app/views", @view_name, "_wcal.html.erb")
-      m.template "weekly_vertical_calendar.scss", File.join("app/stylesheets", @view_name, "weekly_vertical_calendar.scss")
-    end
+    @view_name = "calendar"
+    #    record do |m|
+      template "index.html.erb", File.join("app/views", @view_name, "index.html.erb")
+      template "_wcal.html.erb", File.join("app/views", @view_name, "_wcal.html.erb")
+      template "weekly_vertical_calendar.scss", File.join("app/stylesheets", @view_name, "weekly_vertical_calendar.scss")
+      #    end
   end
 end
