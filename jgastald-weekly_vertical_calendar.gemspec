fadc50f0e4c4c151b1ca56dba3f1e38d018c929b
Jorge Gastaldi <jgastald@gmail.com>
2011-11-26 19:09:48 -0200

    Added model_name generator parameter.

--- jgastald-weekly_vertical_calendar.gemspec
+++ jgastald-weekly_vertical_calendar.gemspec
@@ -10,8 +10,8 @@ Gem::Specification.new do |s|
   s.authors     = ["jgastald"]
   s.email       = ["jgastald@gmail.com"]
 #  s.homepage    = "TODO"
-  s.summary     = "TODO: Summary of JgastaldWeeklyVerticalCalendar."
-  s.description = "TODO: Description of JgastaldWeeklyVerticalCalendar."
+  s.summary     = "Weekly Vertical Calendar"
+  s.description = "It's a Weekly Vertical Calendar."
 
   s.files = Dir["{app,config,db,lib}/**/*"] + ["MIT-LICENSE", "Rakefile", "README.rdoc"]
   s.test_files = Dir["test/**/*"]
--- lib/generators/weekly_vertical_calendar/weekly_vertical_calendar_generator.rb
+++ lib/generators/weekly_vertical_calendar/weekly_vertical_calendar_generator.rb
@@ -13,9 +13,10 @@ class WeeklyVerticalCalendarGenerator < Rails::Generators::NamedBase
   
   def manifest
     @view_name = "weekly_calendar"
+    @model_name = name
     template "index.html.erb", File.join("app/views", @view_name, "index.html.erb")
     template "_wcal.html.erb", File.join("app/views", @view_name, "_wcal.html.erb")
-    template "weekly_calendar_controller.rb", File.join("app/controllers", "weekly_calendar_controller.rb")      
+    template "weekly_calendar_controller.rb.erb", File.join("app/controllers", "weekly_calendar_controller.rb")      
     template "weekly_calendar_helper.rb", File.join("app/helpers", "weekly_calendar_helper.rb")
     
     route "match '/weekly_calendar' => 'weekly_calendar#index'"
